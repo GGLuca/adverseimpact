39473ba82c631a05fa3054350d4aae9b7dfdf163
GGLuca <george.gunnesch-luca@fau.de>
2023-01-14 22:18:54 +0100

    Package rewrite

---
+++ R/advserseimpact-package.R
@@ -0,0 +1,7 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats pnorm
+## usethis namespace: end
+NULL
---
+++ R/ai_ztest.R
@@ -0,0 +1,22 @@
+ai_ztest <- function(NFmin, NPmin, NFmaj, NPmaj) {
+
+  # calculate the total sample size of the minority group and majority group
+  Nmin <- NFmin + NPmin # number of individuals in the minority group
+  Nmaj <- NFmaj + NPmaj # number of individuals in the majority group
+
+  # calculate the proportion of individuals with the feature in the minority and majority groups
+  p1 <- NPmin / Nmin # proportion of individuals with feature in minority group
+  p2 <- NPmaj / Nmaj # proportion of individuals with feature in majority group
+
+  # calculate overall proportion of individuals with the feature
+  p <- (NPmin + NPmaj) / (Nmin + Nmaj)
+
+  # calculate the Z-score using the formula
+  z <- (p1 - p2) / sqrt(p * (1 - p) * ((1/Nmin) + (1/Nmaj)))
+
+  # calculate the p-value assuming a two-tailed test
+  p_value <- 2 * (1 - pnorm(abs(z)))
+
+  # return the Z-score and p-value as a list
+  return(data.frame(z_score = z, p_value = p_value))
+}
