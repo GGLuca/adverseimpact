4182e4e31c530e88bdc1a3ee7076e696f4298a1f
GGLuca <george.gunnesch-luca@fau.de>
2023-04-15 13:28:17 +0300

    Updates to documentation

--- R/ai_adj_shortfall.R
+++ R/ai_adj_shortfall.R
@@ -1,16 +1,26 @@
 #' Adjusted Shortfall
 #'
-#' Further documentation goes here:
-#'
 #' @param NFmin Number of participants failing the selection procedure from the minority group
 #' @param NPmin Number of participants passing the selection procedure from the minority group
 #' @param NFmaj number of participants failing the selection procedure from the majority group
 #' @param NPmaj Number of participants passing the selection procedure from the majority group
 #'
-#' @return
+#' @return A numeric value representing the adjusted shortfall (SFadj) calculated as the
+#'   shortfall (SF) divided by the total number of minority applicants (Nmin).
+#'
 #' @export
 #'
 #' @examples
+#' # Example data
+#' NFmin <- 10 # Number of participants failing the selection procedure from the minority group
+#' NPmin <- 5  # Number of participants passing the selection procedure from the minority group
+#' NFmaj <- 30 # Number of participants failing the selection procedure from the majority group
+#' NPmaj <- 15 # Number of participants passing the selection procedure from the majority group
+#'
+#' # Calculate the adjusted shortfall
+#' result <- ai_shortfall(NFmin, NPmin, NFmaj, NPmaj)
+#' result
+
 ai_shortfall <- function(NFmin, NPmin, NFmaj, NPmaj) {
   # Calculate the total number of minority (Nmin) and majority (Nmaj) applicants
   Nmin <- NFmin + NPmin
--- R/ai_ir.R
+++ R/ai_ir.R
@@ -1,14 +1,28 @@
-#' Computes the
+#' Computes the Impact Ratio (IR) between minority and majority groups
+#'
+#' This function calculates the Impact Ratio (IR) by dividing the selection rate of the minority group (SRmin)
+#' by the selection rate of the majority group (SRmaj). The IR helps assess the relative difference in selection
+#' rates between the two groups.
 #'
 #' @param NFmin Number of participants failing the selection procedure from the minority group
 #' @param NPmin Number of participants passing the selection procedure from the minority group
-#' @param NFmaj number of participants failing the selection procedure from the majority group
+#' @param NFmaj Number of participants failing the selection procedure from the majority group
 #' @param NPmaj Number of participants passing the selection procedure from the majority group
 #'
-#' @return A vector containing the result
+#' @return A numeric value representing the Impact Ratio (IR) between the minority and majority groups.
 #' @export
 #'
 #' @examples
+#' # Example data
+#' NFmin <- 10 # Number of participants failing the selection procedure from the minority group
+#' NPmin <- 5  # Number of participants passing the selection procedure from the minority group
+#' NFmaj <- 30 # Number of participants failing the selection procedure from the majority group
+#' NPmaj <- 15 # Number of participants passing the selection procedure from the majority group
+#'
+#' # Calculate the Impact Ratio
+#' result <- ai_ir(NFmin, NPmin, NFmaj, NPmaj)
+#' result
+
 ai_ir <- function(NFmin, NPmin, NFmaj, NPmaj){
   Nmin <- NFmin + NPmin
   Nmaj <- NFmaj + NPmaj
