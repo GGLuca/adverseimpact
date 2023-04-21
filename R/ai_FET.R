41d4e50663cc1970eb83b888be11311f68c89a36
GGLuca <george.gunnesch-luca@fau.de>
2023-04-21 21:43:35 +0200

    Update

---
+++ R/ai_FET.R
@@ -0,0 +1,37 @@
+#' Compute the Fisher's Exact Test (FET) Statistic
+#'
+#' This function calculates the Fisher's Exact Test (FET) statistic, a measure used
+#' in adverse impact analysis. The FET is a statistical test used to determine if
+#' there are nonrandom associations between two categorical variables.
+#'
+#' @param NFmin The number of non-favored outcomes for the minority group
+#' @param NPmin The number of favored outcomes for the minority group
+#' @param NFmaj The number of non-favored outcomes for the majority group
+#' @param NPmaj The number of favored outcomes for the majority group
+#'
+#' @return A numeric value representing the FET statistic
+#' @export
+#'
+#' @examples
+#' NFmin <- 25
+#' NPmin <- 75
+#' NFmaj <- 100
+#' NPmaj <- 300
+#' ai_FET(NFmin, NPmin, NFmaj, NPmaj)
+#'
+#' @importFrom stats dhyper
+ai_FET <- function(NFmin, NPmin, NFmaj, NPmaj) {
+  # Calculate the total number of minority and majority group members
+  Nmin <- NFmin + NPmin
+  Nmaj <- NFmaj + NPmaj
+  NPt <- NPmin + NPmaj
+
+  # Calculate the Fisher's Exact Test statistic (FEThyp)
+  FEThyp <- 0
+  for (i in 0:NPmin) {
+    FEThyp <- FEThyp + dhyper(NPmaj + i, Nmaj, Nmin, NPt)
+  }
+  FEThyp <- FEThyp * 2
+
+  return(FEThyp)
+}
