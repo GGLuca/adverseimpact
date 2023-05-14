083386ea4c843b8b324fda0cf86a776a57f12986
GGLuca <george.gunnesch-luca@fau.de>
2023-05-14 12:25:57 +0300

    Update for the chisq function

--- R/ai_chi.R
+++ R/ai_chi.R
@@ -9,19 +9,25 @@
 #' @param NPmaj Number of selected individuals in the majority group
 #'
 #' @return The computed Chi-square statistic and associated probability level
+#' @importFrom stats pchisq
 #' @export
 #'
 #' @examples
 #' ai_chi(40, 60, 25, 75)
 #' ai_chi(1771, 338, 25, 532)
 #'
-ai_chi    <- function(NFmin, NPmin, NFmaj, NPmaj) {
-  Nmin    <- NFmin + NPmin
-  Nmaj    <- NFmaj + NPmaj
-  NFt     <- NFmin + NFmaj
-  NPt     <- NPmin + NPmaj
-  N       <- NFt + NPt
+ai_chi <- function(NFmin, NPmin, NFmaj, NPmaj) {
+  Nmin <- NFmin + NPmin
+  Nmaj <- NFmaj + NPmaj
+  NFt  <- NFmin + NFmaj
+  NPt  <- NPmin + NPmaj
+  N    <- NFt + NPt
 
-  ChiSq   <- (N * (NFmin * NPmaj - NFmaj * NPmin)^2) / (Nmin * Nmaj * NPt * NFt)
-  return(ChiSq)
+ChiSq  <- (N * (NFmin * NPmaj - NFmaj * NPmin)^2) / (Nmin * Nmaj * NPt * NFt)
+
+  # Calculate p-value
+  df   <- 1  # degrees of freedom for a 2x2 contingency table
+  p_value <- 1 - pchisq(ChiSq, df)
+
+  return(list(ChiSq=ChiSq, p_value=p_value))
 }
